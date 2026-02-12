20ed3040f2252d72001312c8c96003fe50c4fcf4
Brayan Landa <landakkpp@gmail.com>
2026-02-11 20:02:01 -0500

    Add new product images for various items in the static directory
    - Added multiple new image files for products, enhancing the visual representation of the inventory.
    - Included images in various resolutions and formats to ensure compatibility and quality across platforms.

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,10 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -9,10 +12,24 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { filterFile, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -30,8 +47,9 @@ export class FilesController {
         'Make sure that the file is an image with a valid extension (jpg, jpeg, png, gif)',
       );
     }
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
     return {
-      fileName: file.originalname,
+      secureUrl,
     };
   }
 }
